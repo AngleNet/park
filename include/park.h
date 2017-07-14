51fefd67db98907fc47e7b4d3e7bd27a78101777
anglenet <anglenet@sina.cn>
2017-07-14 17:47:23 +0800

    Add initial blueprint

---
+++ include/park.h
@@ -0,0 +1,8 @@
+//Copyright 2017 The Park Authors. All rights reserved.
+namespace park{
+} // namespace park
+#include "park/function.h"
+#include "park/parameter.h"
+#include "park/rpcclient.h"
+#include "park/rpcserver.h"
+#include "park/network.h"
